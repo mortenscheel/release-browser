0fb49b94677c192345996db62f42f1cb1e958d22
Morten Scheel <morten@mortenscheel.com>
2022-05-21 01:04:46 +0200

    add scout

--- app/Models/Release.php
+++ app/Models/Release.php
@@ -5,15 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Scout\Searchable;
 
 class Release extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $fillable = ['version', 'body'];
 
     public function repo(): BelongsTo
     {
         return $this->belongsTo(Repo::class);
+    }
+
+    public function toSearchableArray()
+    {
+        return $this->only(['version', 'body']);
     }
 }
